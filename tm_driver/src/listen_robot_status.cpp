c69027d883e26935676bbf43dd05b5a16f022dc1
jufengwu <leowu102000@gmail.com>
2019-10-06 16:45:27 +0800

    topic experiment successful

--- tm_driver/src/listen_robot_status.cpp
+++ tm_driver/src/listen_robot_status.cpp
@@ -1,12 +1,18 @@
 #include"../include/listen_robot_status.hpp"
 
+using namespace std::chrono_literals;
+
 namespace tm_driver{
   void ListenRobotSatus::listen_thread(rclcpp::Node::SharedPtr node){
     
     auto callBack =[this](const tm_msgs::msg::RobotStatus::SharedPtr robotStatus) -> void
         {
+          std::cout<<"I hear something"<<std::endl;
           for(int i=0;i<jointNumber;i++){
+            //std::cout<<"i is "<< i <<std::endl;
+            //std::cout<<"robotStatus->current_joint_position is"<<robotStatus->current_joint_position[i]<<std::endl;
             jointPostion[i] = robotStatus->current_joint_position[i];
+            //std::cout<<"jointPostion[i] is"<<jointPostion[i]<<std::endl;
             jointVelocity[i] = robotStatus->current_joint_velocity[i];
             jointTorque[i] = robotStatus->current_joint_force[i];
           }
@@ -51,15 +57,15 @@ int main(int argc, char * argv[]){
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("MyMsgSuber");
   std::unique_ptr<tm_driver::ListenRobotSatus> listenRobotSatus = std::make_unique<tm_driver::ListenRobotSatus>(node,0.5);
-  rclcpp::Rate sleepRate(1);
+  rclcpp::Rate sleepRate(1s);
   while(true){
       auto position = listenRobotSatus->get_joint_position();
       auto vleocity = listenRobotSatus->get_joint_velocity();
       auto torque = listenRobotSatus->get_joint_torque();
-      std::cout<<"--------------------------"<<std::endl;
+      std::cout<<"--------------main loop------------"<<std::endl;
       std::cout<<"joint position is"<<position[0]<<","<<position[1]<<","<<position[2]<<","<<position[3]<<","<<position[4]<<","<<position[5]<<std::endl;
       std::cout<<"joint vleocity is"<<vleocity[0]<<","<<vleocity[1]<<","<<vleocity[2]<<","<<vleocity[3]<<","<<vleocity[4]<<","<<vleocity[5]<<std::endl;
-      std::cout<<"joint position is"<<torque[0]<<","<<torque[1]<<","<<torque[2]<<","<<torque[3]<<","<<torque[4]<<","<<torque[5]<<std::endl;
+      std::cout<<"joint torque is"<<torque[0]<<","<<torque[1]<<","<<torque[2]<<","<<torque[3]<<","<<torque[4]<<","<<torque[5]<<std::endl;
       sleepRate.sleep();
   }
 }
--- tm_gazebo_plugin/src/tm_joint_plugin.cpp
+++ tm_gazebo_plugin/src/tm_joint_plugin.cpp
@@ -24,28 +24,38 @@ namespace gazebo_plugins
     //tm_msgs::msg::RobotStatus motor_status_msg;
 
     auto motorStatusMsg = std::make_shared<tm_msgs::msg::RobotStatus>();
-    
-    std::cout<<"send position"<<std::endl;
-    motorStatusMsg->current_joint_position.clear();
-    motorStatusMsg->current_joint_velocity.clear();
-    motorStatusMsg->current_joint_force.clear();
+    rclcpp::WallRate loop_rate(1s);
+    motorStatusMsg->current_joint_position.resize(jointNumber);
+    motorStatusMsg->current_joint_velocity.resize(jointNumber);
+    motorStatusMsg->current_joint_force.resize(jointNumber);
+    while (rclcpp::ok())
+    {
+      std::cout<<"send position"<<std::endl;
+      motorStatusMsg->current_joint_position.clear();
+      motorStatusMsg->current_joint_velocity.clear();
+      motorStatusMsg->current_joint_force.clear();
 
-    for(unsigned int i=0;i<this->jointNumber;i++){
-      std::cout<<"gazeboJoint[i]->Position(0) is "<<gazeboJoint[i]->Position(0)<<std::endl;
-      motorStatusMsg->current_joint_position.push_back(gazeboJoint[i]->Position(0)); //it will push_back to many and not release
-      //std::cout<<","<<gazeboJoint[i]->Position(0);
-      //motor_status_msg.current_joint_velocity.push_back(gazeboJoint[i]->GetVelocity(0));
-      //motor_status_msg.current_joint_force.push_back(gazeboJoint[i]->GetForce(0));
-    } 
+      for(unsigned int i=0;i<this->jointNumber;i++){
+        //std::cout<<"gazeboJoint[i]->Position(0) is "<<gazeboJoint[i]->Position(0)<<std::endl;
+        motorStatusMsg->current_joint_position.push_back(gazeboJoint[i]->Position(0)); //it will push_back to many and not release
+        //std::cout<<","<<gazeboJoint[i]->Position(0);
+        motorStatusMsg->current_joint_velocity.push_back(gazeboJoint[i]->GetVelocity(0));
+        motorStatusMsg->current_joint_force.push_back(gazeboJoint[i]->GetForce(0));
+      } 
+      loop_rate.sleep();
+      motorStatusPublish->publish(*motorStatusMsg);
+    }
     
-    motorStatusPublish->publish(*motorStatusMsg);
+    
+   
   }
   void TMGazeboPluginRosPrivate::create_topic(){
     
 
     motorStatusPublish = rosNode->create_publisher<tm_msgs::msg::RobotStatus>("tm_motor_states",rclcpp::QoS(100));
     
-    motorStatusPublishTimer = rosNode->create_wall_timer(1s,std::bind(&TMGazeboPluginRosPrivate::message_publish, this));
+    //motorStatusPublishTimer = rosNode->create_wall_timer(1s,std::bind(&TMGazeboPluginRosPrivate::message_publish, this));
+    std::thread(&TMGazeboPluginRosPrivate::message_publish,this).detach();
   }
 
 
